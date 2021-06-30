7a319e0884195c9d2b77f2690133ec1052fcd513
phobo22 <86713733+phobo22@users.noreply.github.com>
2021-06-30 16:25:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
     introTitle: 'Bạn iu à!',
-    introDesc: `Hôm nay là sn m đó
-    Mà t chả biết mua cái gì cho m cả
-    Dịch thì quá chời
+    introDesc: `Hôm nay là sn m đó.
+    Mà t chả biết mua cái gì cho m cả.
+    Dịch thì quá chời, ko đi chơi đc.
     Tui chỉ biết làm như này, để nói là:
     Happy Birthday Broo!!!
     Mong la bạn sẽ ko bùn tui :))`,
     btnIntro: 'hihi',
-    title: 'T hỏi thiệt 😙',
+    title: 'T hỏi thiệt �',
     desc: 'M có pùn t hem???? ',
     btnYes: 'Sao mà dận bạn iu dấu của t được <33',
     btnNo: 'Có nha, Dận tím người :3',
@@ -15,7 +15,7 @@ const CONFIG = {
     btnReply: 'Gửi cho tao :)',
     reply: 'M quá cute <33333333',
     mess: 'Tao biết mà 🥰. Hạnh phúc quá đi 😘😘',
-    messDesc: 'Hỏi lần cuối. Tao khỏi tặng quà m nhoa.',
+    messDesc: 'Mà t khỏi tặng quà m nhoa!!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100042071405849'
 }
