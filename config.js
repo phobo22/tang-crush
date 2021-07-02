f593a85180c6d54460190960815f5de043144f0f
phobo22 <86713733+phobo22@users.noreply.github.com>
2021-07-02 19:27:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tâm à!',
+    introTitle: 'Hii Bạn iu dấu!!',
     introDesc: `Hôm nay là sn m đó.
-    Mà t chả biết mua cái gì cho m cả.
-    Dịch thì quá chời, ko đi chơi đc.
-    Tui chỉ biết làm như này, để nói là:
-    Happy Birthday Broo!!!
-    Mong là bạn sẽ ko bùn tui :))`,
+    Happy Birthday Broo!!!`,
     btnIntro: 'hihi',
-    title: 'T hỏi thiệt �',
-    desc: 'M có dận t hem???? ',
-    btnYes: 'Sao mà dận bạn iu dấu của t được <33',
-    btnNo: 'Có nha, Dận tím người :3',
+    title: 'T hỏi này nè ~~ �',
+    desc: 'T ko tặng quà được hok dọ???? ',
+    btnYes: 'Khỏi cũm được, hỏng seooo <33',
+    btnNo: 'Phải tặng :( :3',
     question:'M có pùn t hơm??? <3',
     btnReply: 'Gửi cho tao :)',
-    reply: 'Hong bùn xíu nào <33333333',
-    mess: 'Tao biết mà 🥰. Hạnh phúc quá đi 😘😘',
-    messDesc: 'Mà t khỏi tặng quà m nhoa!!!',
+    reply: 'Hong bùn xíu nào hết á <3333',
+    mess: 'Tao biết mà <3. Tks bạn iu nhìu nhìu nhìu 😘😘',
+    messDesc: 'HAPPY BIRTHDAY --- TÂM TRẦN--- !!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100042071405849'
 }
